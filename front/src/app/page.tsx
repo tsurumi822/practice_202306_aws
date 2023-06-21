945c55b83858085df0d082802eb4a1844c4dfb8d
tsurumi822 <nozomi.tsurumi@divx.co.jp>
2023-06-21 09:48:01 +0900

    local to remote

---
+++ front/src/app/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState, useEffect } from "react";
+
+const HomePage: React.FC = () => {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/api/hello");
+        const data = await res.json();
+        setMessage(data.message);
+      } catch (error) {
+        setMessage("エラーが発生しました。");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default HomePage;
